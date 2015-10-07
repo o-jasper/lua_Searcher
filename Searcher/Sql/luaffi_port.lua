ddd2faea0cb48cefdb0170290370487ab69155de
Jasper den Ouden <o.jasper@gmail.com>
2015-10-07 14:13:44 +0200

    More assertions, mostly.. Cant seem to get rid of segfaults..

--- Searcher/Sql/luaffi_port.lua
+++ Searcher/Sql/luaffi_port.lua
@@ -4,6 +4,10 @@ local sql3 = require "Searcher.Sql.luaffi"
 local This = {}
 This.__index = This
 
+local code = sql3.code
+local OK, DONE, ROW, BUSY = code.OK, code.DONE, code.ROW, code.BUSY
+local FLOAT, INTEGER, TEXT = code.CODE, code.INTEGER, code.TEXT
+
 function This:new(new)
    new = setmetatable(assert(new), self)
    new:init()
@@ -13,19 +17,23 @@ function This:init()
    self.cdata_ptr = ffi.new("sqlite3*[1]")
    local err = sql3.open(self.filename, self.cdata_ptr)
    self.cdata = self.cdata_ptr[0]
-   self.err = (err ~= sql3.code.OK and err) or nil
+   self.err = (err ~= OK and err) or nil
 end
 
 local function rawcall(stmt, input)
    local cdata = stmt
-   for i, el in ipairs(input) do
+   assert(sql3.bind_parameter_count(cdata) == #input)  -- Seems to crash here already?
+   for i, el in ipairs(input) do  -- Put in values.
+      el = tonumber(el) or el
       if type(el) == "string" then
          sql3.bind_text(cdata, i, el, #el, nil)
+      elseif el == nil then
+         sql3.bind_null(cdata, i)
       elseif not type(el) == "number" then
          error(string.format("Dunno what to do with type: %s", type(el)))
       else
          if el%1 == 0 then
-               sql3.bind_int(cdata, i, el)
+            sql3.bind_int(cdata, i, el)
          else
             sql3.bind_double(cdata, i, el)
          end
@@ -33,21 +41,19 @@ local function rawcall(stmt, input)
    end
 
    local rc, ret = sql3.step(cdata), {}
-   local function grabstep()
-      while rc == sql3.code.ROW do
-         -- Grab the row.
+   local function grabstep()  -- Grabs a row.
+      while rc == ROW do
          local here = {}
          for i = 0, sql3.column_count(cdata) - 1 do
             local tp, got = sql3.column_type(cdata, i), nil
-            if tp == sql3.code.FLOAT then
-               got = sql3.column_double(cdata, i)
-            elseif tp == sql3.code.INTEGER then
-               got = sql3.column_int64(cdata, i)
-            elseif tp == sql3.code.TEXT then
+            if tp == FLOAT then
+               got = tonumber(sql3.column_double(cdata, i))
+            elseif tp == INTEGER then
+               got = tonumber(sql3.column_int64(cdata, i))
+            elseif tp == TEXT then
                got = ffi.string(sql3.column_text(cdata, i))
             end
             here[ffi.string(sql3.column_name(cdata, i))] = got
-            --print(sql3.column_name(cdata, i))
          end
          table.insert(ret, here)
 
@@ -56,36 +62,43 @@ local function rawcall(stmt, input)
       return rc  -- Return whatever remains.
    end
 
-   while grabstep() == sql3.code.BUSY do end
+   -- While grabbing rows, BUSY is allowed.
+   while grabstep() == BUSY do end
 
-   if rc ~= sql3.code.DONE then
+   -- At end must be done, or must be some error.
+   if rc ~= DONE then
       print("ERR", rc, unpack(sql3.invcode[rc]))
       return
    end
    return ret
 end
 
+-- Makes a statement.
 local function rawprep(cdata, statement)
    local cdata_ptr = ffi.new("sqlite3_stmt*[1]")
-   local rc = sql3.prepare_v2(cdata, statement, #statement, cdata_ptr, nil)
-   return cdata_ptr, cdata_ptr[0]
+   --print("***", cdata_ptr, cdata_ptr[0], statement)
+   local pzTail = ffi.new("const char*[1]")  --TODO
+   local rc = sql3.prepare_v2(cdata, statement, #statement + 1, cdata_ptr, pzTail)
+   assert(rc == OK)
+   return cdata_ptr
 end
 
 function This:compile(statement)
-   local _, cdata = rawprep(self.cdata, statement)
+   assert(sql3.complete(statement) ~= 0, "Incompete:\n" .. statement)
+   local cdata = rawprep(self.cdata, statement)
    return function(...)
-      local ret = rawcall(cdata, {...})
-      sql3.reset(cdata)
-      sql3.clear_bindings(cdata)
+      local ret = rawcall(cdata[0], {...})
+      assert( sql3.reset(cdata[0]) == OK )
+      assert( sql3.clear_bindings(cdata[0]) == OK )
       return ret
    end
 end
 
 function This:exec(statement, ...)
-   local _, cdata = rawprep(self.cdata, statement)
-   local ret = rawcall(cdata, {...})
-   sql.finish(data)  -- This one is destroyed.
-   -- TODO free it?
+   assert(sql3.complete(statement .. ";") ~= 0)
+   local cdata = rawprep(self.cdata, statement .. ";")
+   local ret = rawcall(cdata[0], {...})
+   assert( sql3.finalize(cdata[0]) == OK )  -- This one is destroyed.
    return ret
 end
 
