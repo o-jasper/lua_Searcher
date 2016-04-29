147735b106e49cc88dd3428040dc4df44c2779a1
Jasper den Ouden <o.jasper@gmail.com>
2016-04-29 17:15:49 +0200

    `prep_str`

--- Searcher/Sql/command_str.lua
+++ Searcher/Sql/command_str.lua
@@ -1,4 +1,5 @@
 local function sql_command_str(sql_pattern, input, prep_string)
+   prep_string = prep_string or function(r) return r end
    local i = 0
    local function ret_one()
       i = i + 1
