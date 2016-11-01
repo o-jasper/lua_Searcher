eaf8c1c8a172dd5e477434febf211ce06d042eac
Jasper den Ouden <o.jasper@gmail.com>
2016-11-01 01:55:27 +0100

    Separate out the part that just produces Sql strings.

--- Searcher/db/Sql/Filter/init.lua
+++ Searcher/db/Sql/Filter/init.lua
@@ -5,47 +5,17 @@
 --  by the Free Software Foundation, either version 3 of the License, or
 --  (at your option) any later version.
 
-local sql_filters = require "Searcher.db.Sql.Filter.raw.filters"
 local EntryMeta = require "Searcher.db.Sql.Filter.raw.EntryMeta"
 
-local maclike = require "Searcher.util.maclike"
-
-local This = require("Searcher.db.raw.BaseFilter"):class_derive{
-   __name="Searcher.db.Sql.Filter"
-}
+local This = require("Searcher.db.raw.BaseFilter"):class_derive(
+   require "Searcher.db.Sql.Filter.JustSqlString",
+   { __name="Searcher.db.Sql.Filter" })
 
 This.description = [[Sql filter object, the creator specifies the search term.
 Better create via `Searcher.db.Sql`.]]
 
 function This:init()
-   self._sql, self._search, self._delete = {}, {}, {}
-end
-
-function This:sql(kind)
-   assert(self.kinds and kind)
-   return maclike(
-      {depth=0, kind=kind, kind_name=kind.name, kinds=self.kinds},
-      sql_filters,
-      self)
-end
-
--- TODO filters selecting subset of kinds.
-function This:search_sql(kind_name)
-   local kind = assert(self.kinds[kind_name], "Could not figure kind: " .. kind_name)
-   local statement = self:sql(kind)
-   statement = (statement == "TRUE" and "" or " d0 WHERE\n") .. statement
-
-   local sql = "SELECT * FROM " .. kind._sql_name .. statement
-
-   local order_by = self:figure_order_by(kind)
-   if order_by ~= "false" then
-      sql = sql .. "\nORDER BY " .. order_by .. (self.desc and " DESC" or "")
-   end
-   local limit_cnt = self:figure_limit_cnt(kind)
-   if limit_cnt >=0 then
-      sql = sql .. "\nLIMIT " .. (self.limit_from or 0) .. " " .. limit_cnt
-   end
-   return sql .. ";"
+   self._search, self._delete = {}, {}
 end
 
 local function raw_search_fun(self, kind_name)
